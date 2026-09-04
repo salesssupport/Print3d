3a070c5ce0e6fad4fd81f1bc671d8bfa3a4e2352
salesssupport <salesssupport@atomicmail.io>
2026-09-04 18:37:14 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -75,7 +75,8 @@ function listenToRoom(){
 
 async function enter(){
   const key=cleanRoomId(roomKeyInput.value);
-  if(key.length<5){
+  //if(key.length<5){
+  if(key.value!='Luna23'){
     loginError.textContent="No results found..";
     return;
   }
